c0ee1c149bb86f61cd0448c895ab93fec99afab0
Meher Slimani <meher.slimeni@gmail.com>
2021-01-15 03:14:18 +0100

    Creating the user registration route

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+//Define Routes
+app.use("/api/users", require("./routes/api/users.js"));
+app.use("/api/auth", require("./routes/api/auth.js"));
+app.use("/api/profile", require("./routes/api/profile.js"));
+app.use("/api/posts", require("./routes/api/posts.js"));
 
 const PORT = process.env.PORT || "5000";
 
