380b7f34a1ac37c9a512314b544df6e80ffb0054
Meher Slimani <meher.slimeni@gmail.com>
2021-01-22 22:55:36 +0100

    Add Experience & Educate display

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
+import Education from "./Education";
 
 const Dashboard = () => {
   const auth = useSelector((state) => state.auth);
@@ -27,6 +29,8 @@ const Dashboard = () => {
       {profile !== null ? (
         <>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </>
       ) : (
         <>
