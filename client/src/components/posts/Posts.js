58f297348917bfd3bdecf1d22282f5ddbc404c4e
Meher Slimani <meher.slimeni@gmail.com>
2021-01-23 22:59:35 +0100

    Add postItem component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "../../actions/post";
 import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 
 const Posts = () => {
   const post = useSelector((state) => state.post);
@@ -11,7 +12,22 @@ const Posts = () => {
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch]);
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className="large text-primary"></h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome to the community
+      </p>
+      {/* Post form */}
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Posts;
